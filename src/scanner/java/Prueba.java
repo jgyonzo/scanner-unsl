6b4fde6e5eda97df0ae3d837482ab47377c7a5a6
jgyonzo <juan.yonzo@mercadolibre.com>
2011-05-02 00:41:46 -0300

    jettison.jar added to /lib, fixed javaconf.json

--- src/scanner/java/Prueba.java
+++ src/scanner/java/Prueba.java
@@ -1,15 +1,41 @@
 package scanner.java;
 
-import java.util.List;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Map;
 
-import scanner.token.Token;
+import org.codehaus.jettison.json.JSONObject;
 
 
 public class Prueba {
 	public static void main(String[] args) throws Exception{
-		String forscan = "aa.b10 ";
-		JavaScanner scanner = new JavaScanner();
-		List<Token> tokens = scanner.scan(forscan);
-		System.out.println(tokens);
+//		String forscan = "aa.b10 ";
+//		JavaScanner scanner = new JavaScanner();
+//		List<Token> tokens = scanner.scan(forscan);
+//		System.out.println(tokens);
+		
+		String json = Prueba.readFileAsString("javaconf.json");
+		JSONObject jsonOb = new JSONObject(json);
+		JSONObject delta = (JSONObject)jsonOb.get("delta");
+		System.out.println(jsonOb.toString());
+	}
+	
+	/**
+	 * @param filePath
+	 *            the name of the file to open
+	 */
+	private static String readFileAsString(String filePath)
+			throws java.io.IOException {
+		StringBuffer fileData = new StringBuffer(1000);
+		BufferedReader reader = new BufferedReader(new FileReader(filePath));
+		char[] buf = new char[1024];
+		int numRead = 0;
+		while ((numRead = reader.read(buf)) != -1) {
+			String readData = String.valueOf(buf, 0, numRead);
+			fileData.append(readData);
+			buf = new char[1024];
+		}
+		reader.close();
+		return fileData.toString();
 	}
 }
